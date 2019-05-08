fadd7cb49e2fdb4bfb2cd2d4b636b5dab88bc52f
Terje Torkelsen <terje.torkelsen@avinor.no>
2019-05-08 16:32:03 +0200

    Removed unused script

--- scripts/terraform.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Install 0.12 version since build agent does not have that currently
-#
-
-TERRAFORM_VERSION=0.12.0-rc1
-curl -LO "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
-unzip "terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -d /usr/local/bin
-rm -f "terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
